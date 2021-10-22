e70afb0fec866811b6e42ee7f0352d51c81faab2
vanderL <vnd.vander@gmail.com>
2021-10-21 21:59:59 -0300

    Config Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>Oi mundo</div>
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    email: '',
+}
+
+export const userReducer = (state = initialState, action) => {
+    
+    if(action.type === 'SET_EMAIL') {
+        return {...state, email: action.payload.email }
+    }
+
+    return state
+}
