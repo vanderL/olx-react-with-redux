cb12a1ce438cb8e7b5adc295800613a5d3c713f6
vanderL <vnd.vander@gmail.com>
2021-10-25 18:53:53 -0300

    Creating the Header

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { HeaderArea } from './styles';
 
+import { isLogged } from '../../../helpers/AuthHandler';
+
 function Header() {
+  console.log(isLogged)
   return (
     <HeaderArea>
-        ...
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <span className="logo-1">O</span>
+              <span className="logo-2">L</span>
+              <span className="logo-3">X</span>
+            </Link>
+          </div>
+          <nav>
+            <ul>
+            {
+              isLogged() ? (
+                <>
+                 <li>
+                    <Link to="/my-account"> Minha Conta </Link>
+                  </li>
+                  <li>
+                    <Link to="/logout"> Sair </Link>
+                  </li>
+                  <li>
+                    <Link to="/post-an-ad" className="button"> Poste Um Anuncio </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link to=""> Login </Link>
+                  </li>
+                  <li>
+                    <Link to=""> Cadastrar </Link>
+                  </li>
+                  <li>
+                    <Link to="/singin" className="button"> Poste Um Anuncio </Link>
+                  </li>
+                </>
+              )
+            }
+            </ul>
+          </nav>
+        </div>
     </HeaderArea>
   )
 }
--- src/components/partials/Header/styles.js
+++ src/components/partials/Header/styles.js
@@ -1,6 +1,76 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #ccc;
+    background-color: #fff;
+    height: 60px;
+    border-bottom: 1px solid #ccc;
+
+    .container {
+        max-width: 1000px;
+        margin: auto;
+        display: flex;
+    }
+
+    .logo {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        height: 60px;
+
+        .logo-1,
+        .logo-2,
+        .logo-3 {
+            font-size: 27px;
+            font-weight: bold;
+        }
+
+        .logo-1 {
+            color: #ff0000;
+        }
+
+        .logo-2 {
+            color: #00ff00;
+        }
+
+        .logo-3 {
+            color: #0000ff;
+        }
+    }
+
+    nav {
+        padding-top: 10px;
+        padding-bottom: 10px;
+
+        ul {
+            display: flex;
+            align-items: center;
+            height:  40px;
+        }
+
+        li {
+            margin-left: 20px;
+            margin-right: 20px;
+
+            a {
+                color: #111;
+                font-size: 14px;
+                transition: all 0.5s;
+
+                &:hover {
+                    color: #999;
+                }
+
+                &.button {
+                    background-color: #ff8100;
+                    border-radius: 4px;
+                    color: #fff;
+                    padding: 5px 10px;
+
+                    &:hover {
+                        background-color: #e57706;
+                    }
+                }
+            }
+        }
+    }
 `;
